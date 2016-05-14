4736586231c1c725b97796649d596934cc02eb13
Tomasz Juchniewicz <tjuchniewicz@gmail.com>
2016-05-14 09:58:06 +0200

    Add comments about working and not working versions of Spring

--- feign-annotation-test/src/test/java/test/ValidateFeignClientTest.java
+++ feign-annotation-test/src/test/java/test/ValidateFeignClientTest.java
@@ -7,6 +7,8 @@ public class ValidateFeignClientTest {
 
     @Test
     public void findAnnotationTest() {
+		// fails with Spring 4.2.6
+		// works fine with Spring 4.1.9
         AnnotationUtils.findAnnotation(new TestFeignClient().getClass(), FeignClient.class);
     }
 }
